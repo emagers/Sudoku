ba316122e7d24313345cfb9140c34eae0ab3d09c
Eric Magers <eric.magers15@gmail.com>
2019-11-09 14:25:50 -0800

    Update Board.cs

--- SudokuLogic/Board.cs
+++ SudokuLogic/Board.cs
@@ -1,10 +1,19 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using SudokuLogic.Enums;
 
 namespace SudokuLogic
 {
     public class Board : List<List<(int, List<int>)>> 
     {
+        private readonly Dictionary<Difficulty, int> moveCounts = new Dictionary<int, int>
+        {
+             { Difficulty.EASY, 0 },
+             { Difficulty.MEDIUM, 0 },
+             { Difficulty.HARD, 0 },
+             { Difficulty.EXPERT, 0 }
+        };
+
         public Board() { }
 
         public Board(List<List<int>> board)
@@ -13,6 +22,11 @@ namespace SudokuLogic
             {
                 Add(board[i].Select(item => (item, new List<int>())).ToList());
             }
+        }
+
+        public void UpdatePossibilitiesAtPosition(int row, int column, List<int> available, Difficulty reductionDifficilty)
+        {
+            
         }
     }
 }
